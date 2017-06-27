5dcc2f0f7d57c0ef5bc3b423319d13ccafe04aee
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-06-27 15:16:17 +0530

    Integrated with f8-m-p for Istio kube inject

--- helloboot-tracing/service4/src/main/java/org/workspace7/cloudnative/demos/service4/Service4AutoConfiguration.java
+++ helloboot-tracing/service4/src/main/java/org/workspace7/cloudnative/demos/service4/Service4AutoConfiguration.java
@@ -1,5 +1,6 @@
 package org.workspace7.cloudnative.demos.service4;
 
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
  * @author kameshs
  */
 @Configuration
+@AutoConfigureAfter
 public class Service4AutoConfiguration {
 
     @Bean
